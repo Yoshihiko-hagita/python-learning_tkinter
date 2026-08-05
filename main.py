fda47148b5838f3ebe0f3baf2ef040331c219a21
YOSHIHIKO HAGITA <610917hagita@gmail,com>
2026-08-06 02:30:04 +0900

    update

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
+import ctypes
 import tkinter as tk
-import ctypes # 日本語入力時のIMEの文字サイズずれ防止
-
-
-# ==================================
-# 日本語入力時のIMEの文字サイズずれ防止
-# ==================================
-try:
-    ctypes.windll.shcore.SetProcessDpiAwareness(1)
-except Exception:
-    pass
 
 from src.gui.frame_management import FrameManager
+
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+except OSError:
+    pass
+
 
 # ==================================
 # Appクラス
 # ==================================
 
-class App(tk.Tk):
 
+class App(tk.Tk):
     def __init__(self):
 
         super().__init__()
@@ -27,9 +24,6 @@ class App(tk.Tk):
         self.frame_manager.init_frames(self)
         self.frame_manager.show_frame("LogInFrame")
 
-
-
-
     # ==================================
     # ウィンドウ初期化
     # ==================================
@@ -38,7 +32,7 @@ class App(tk.Tk):
         height = 600
 
         # ウィンドウサイズ変更不可
-        self.resizable(False,False)
+        self.resizable(False, False)
 
         # 画面を中央に配置するための座標を取得
         screen_width = self.winfo_screenwidth()
@@ -49,18 +43,18 @@ class App(tk.Tk):
         y = (screen_height - height) // 2
 
         # ウィンドウサイズ&画面を中央に配置
-        self.geometry( f"{width}x{height}+{x}+{y}")
+        self.geometry(f"{width}x{height}+{x}+{y}")
 
         # Excelで例えるとA1セルを画面いっぱいに広げる
-        self.rowconfigure(0,weight=1)
-        self.columnconfigure(0,weight=1)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
 
 # ==================================
 # 起動
 # ==================================
 
 if __name__ == "__main__":
-
     app = App()
 
     app.mainloop()
