ee4f968d9c3697c91ebdb0c2d14472dae405c351
YOSHIHIKO HAGITA <610917hagita@gmail.com>
2026-08-24 10:12:49 +0900

    2026-08-24 10:12:49

--- src/models/equipment.py
+++ src/models/equipment.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 
+# 備品：一覧取得用
 @dataclass
 class Equipment:
     item_id: str
@@ -13,4 +14,18 @@ class Equipment:
     loaned_qty: int
     quantity_per_unit: int | None
     content_unit_name: str | None
+    remarks: str | None
+
+
+# 備品：登録用
+@dataclass
+class EquipmentRegistration:
+    item_name: str
+    item_specification: str | None
+    model_no: str | None
+    category: str
+    quantity: int
+    unit_id: int
+    quantity_per_unit: int | None
+    content_unit_id: int | None
     remarks: str | None
